b52b24db9317cac03d7555bea9a0d4009788aced
riThvik <rahulsingh995871@gmail.com>
2022-07-14 12:14:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var ham=document.querySelector(".ham");
+var btn=document.querySelector("nav img");
+btn.addEventListener("click",()=>{
+//    ham.style.display="inline-block"
+    ham.classList.toggle("dis")
+})
